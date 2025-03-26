c631bbd90463151ca99a5a58e36e880f7fe53149
Wickramasinghe Arachchige Sajith Tharaka <129993532+Sajith22@users.noreply.github.com>
2025-03-26 15:38:59 +0530

    made all neseecery componet

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import connectCloudinary from "./config/cloudinary.js"
 import userRouter from "./routes/userRoute.js"
 import doctorRouter from "./routes/doctorRoute.js"
 import adminRouter from "./routes/adminRoute.js"
+import paymentRoutes from "./routes/paymentRoutes.js";
+
 
 // app config
 const app = express()
@@ -21,6 +23,8 @@ app.use(cors())
 app.use("/api/user", userRouter)
 app.use("/api/admin", adminRouter)
 app.use("/api/doctor", doctorRouter)
+app.use("/api/payments", paymentRoutes)
+
 
 app.get("/", (req, res) => {
   res.send("API Working")
