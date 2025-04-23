f8552a0a27bf5cee436a6988101a155e090856fd
Dhanoshi24 <dhanoshi9924@gmail.com>
2025-04-23 13:48:54 +0530

    update supplyManager

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AdminContextProvider>
       <DoctorContextProvider>
-        <SupplyManagerProvider>
+      <SupplyManagerProvider>
           <AppContextProvider>
             <App />
           </AppContextProvider>
