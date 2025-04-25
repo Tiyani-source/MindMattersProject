97e374cf09a08061d24f6cea4bb8ccf476dcea10
silunakajanuda <silunakajanuda@gmail.com>
2025-04-25 16:13:46 +0530

    Feautures/ Usr Management1

--- backend/routes/patientRoute.js
+++ backend/routes/patientRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import { signup, googleAuth } from "../controllers/patientController.js";
 const patientRouter =  express.Router();
 
+//Comment
 patientRouter.post("/signup", signup);
 patientRouter.post("/google-auth", googleAuth);
 
