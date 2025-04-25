5d27df23cf567a94213e0411c606d73ea257ee6e
silunakajanuda <silunakajanuda@gmail.com>
2025-04-25 18:01:56 +0530

    Revert "Fix: removed secret from .env file"

--- backend/routes/patientRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signup, googleAuth } from "../controllers/patientController.js";
-const patientRouter =  express.Router();
-
-patientRouter.post("/signup", signup);
-patientRouter.post("/google-auth", googleAuth);
-
-export default patientRouter;
