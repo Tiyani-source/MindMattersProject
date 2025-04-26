9cc1462e71ba53990989af097b9477279dd3d525
silunakajanuda <silunakajanuda@gmail.com>
2025-04-26 20:12:37 +0530

    Fully merged code

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom'
 import AdminContextProvider from './context/AdminContext.jsx'
 import DoctorContextProvider from './context/DoctorContext.jsx'
 import AppContextProvider from './context/AppContext.jsx'
 import UniversityContextProvider from './context/UniversityContext.jsx'
 import SupplyManagerProvider from './context/SupplyManagerContext.jsx';
+
 
 console.log("In main.jsx..."),  // Debugging
 
