4a21ade2ec7ce63ff1dbe4488b27c6691d72949c
silunakajanuda <silunakajanuda@gmail.com>
2025-04-26 12:27:16 +0530

    Updated backend by JS

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,18 +1,24 @@
 import express from 'express';
-import { loginAdmin, appointmentsAdmin, appointmentCancel, addDoctor, allDoctors, adminDashboard } from '../controllers/adminController.js';
+import { loginAdmin, appointmentsAdmin, appointmentCancel, addDoctor, allDoctors, adminDashboard,getAdminProfile,updateAdminProfile } from '../controllers/adminController.js';
 import { changeAvailablity } from '../controllers/doctorController.js';
+import {authAdmin }from '../middleware/authAdmin.js';
 import { changeStatus } from '../controllers/orderController.js';
-import authAdmin from '../middleware/authAdmin.js';
+
 import upload from '../middleware/multer.js';
 const adminRouter = express.Router();
 
-adminRouter.post("/login", loginAdmin)
+adminRouter.post("/login", authAdmin)
 adminRouter.post("/add-doctor", authAdmin, upload.single('image'), addDoctor)
 adminRouter.get("/appointments", authAdmin, appointmentsAdmin)
 adminRouter.post("/cancel-appointment", authAdmin, appointmentCancel)
 adminRouter.get("/all-doctors", authAdmin, allDoctors)
 adminRouter.post("/change-availability", authAdmin, changeAvailablity)
+adminRouter.get("/dashboard",  adminDashboard)
+adminRouter.get("/profile", getAdminProfile);
+adminRouter.put("/update-profile", updateAdminProfile)
 adminRouter.post("/change-status", authAdmin, changeStatus)
-adminRouter.get("/dashboard", authAdmin, adminDashboard)
+
+
+
 
 export default adminRouter;
