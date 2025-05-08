88cd1c7ce7bf5bb1aba643c27eb5870446bdab36
Wickramasinghe Arachchige Sajith Tharaka <129993532+Sajith22@users.noreply.github.com>
2025-05-08 17:12:55 +0530

    remove pdf

--- admin/src/pages/Payment/orderPaymentDashboard.jsx
+++ admin/src/pages/Payment/orderPaymentDashboard.jsx
@@ -587,7 +587,7 @@ const downloadPDF = async () => {
               >
                 Export CSV
               </button>
-
+                {/* Download Buttons 
               <button
                 onClick={downloadPDF}
                 className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
@@ -595,6 +595,7 @@ const downloadPDF = async () => {
               >
                 Export PDF
               </button>
+              */}
             </div>
           </div>
         </div>
